aa8b707430b371737c7e572342fe34f0ea15c593
Radif Aghnadiin <radif.aghnadiin@gmail.com>
2025-05-19 15:46:32 +0700

    template express handlebars

---
+++ index.js
@@ -0,0 +1,24 @@
+// const express = require("require");
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "hbs");
+app.set("views", "src/views");
+
+app.use("/assets", express.static("src/assets"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/contact", (req, res) => {
+  const phoneNumber = "08511231234";
+  res.render("contact", { phoneNumber });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+ 
